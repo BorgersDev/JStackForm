9e6ee35ca018f78a940a37b38037a472b971804b
Arthur Borges <borgesarthur.dev@gmail.com>
2025-01-07 14:03:45 -0300

    First Api and modules

---
+++ Node/modules/index.js
@@ -0,0 +1,7 @@
+const {printName, lastName} = require('./printName')
+
+printName(`Arthur ${lastName}`)
+
+const os = require('os');
+
+console.log(os.type())
---
+++ Node/modules/text.js
@@ -0,0 +1,6 @@
+// Módulos: são conjuntos de código.
+
+// 3 tipos de módulos:
+// -> TODOS OS ARQUIVOS JAVASCRIPT SÃO MÓDULOS;
+// -> Nativos; ex: http
+// -> npm (Node Package Manager);
