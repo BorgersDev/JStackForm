e693d1ed99e41684840bc05171ebe6821b9d13ff
Arthur Borges <borgesarthur.dev@gmail.com>
2025-01-10 11:12:21 -0300

    Selecting users by id

--- Node/firstapi/src/controllers/UserController.js
+++ Node/firstapi/src/controllers/UserController.js
@@ -4,7 +4,7 @@ const users = require('../mocks/users')
 module.exports = {
     listUsers(request, response) {
     const { order } = request.query;
-    
+
         const sortedUsers = users.sort(( a , b ) => {
             if (order === 'desc') {
                 return a.id < b.id ? 1 : -1;
@@ -13,5 +13,19 @@ module.exports = {
         })
         response.writeHead(200, { 'Content-Type': 'application/json'});
         response.end(JSON.stringify(users))
+    },
+
+    getUserById(request,response) {
+        const { id } = request.params;
+
+        const user = users.find((user) => user.id === Number(id))
+
+        if(!user) {
+            response.writeHead(400, { 'Content-Type': 'application/json'});
+            response.end(JSON.stringify({ error: 'User not found' }))
+        }
+
+        response.writeHead(200, { 'Content-Type': 'application/json'});
+        response.end(JSON.stringify({ user }))
     }
 }
--- Node/firstapi/src/index.js
+++ Node/firstapi/src/index.js
@@ -6,15 +6,30 @@ const routes = require('./routes');
 const server = http.createServer((request, response) => {
     const parsedUrl = new URL(`http://localhost:3000${request.url}`)
 
-    console.log(`Request method ${request.method} | Endpoint: ${parsedUrl.pathname}`)
+    console.log(`Request method ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+    let {  pathname } = parsedUrl;
+
+    let id = null;
+
+    const splitEndpoint = pathname.split('/').filter(Boolean)
+    console.log(splitEndpoint)
+
+    if(splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`;
+        id = splitEndpoint[1];
+
+    }
 
     const route = routes.find((routeObj) => (
-        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+        routeObj.endpoint === pathname && routeObj.method === request.method
     ));
 
     if (route) {
-        request.query = Object.fromEntries(parsedUrl.searchParams)
-        route.handler(request, response)
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = { id };
+
+        route.handler(request, response);
     } else {
         response.writeHead(404, { 'Content-Type': 'text/html'});
         response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
